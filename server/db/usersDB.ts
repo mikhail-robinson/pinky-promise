8d4ef4064cf9a024d4f7e7191ec933cedbe38012
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-29 11:11:55 +1200

    change order of arguments

--- server/db/usersDB.ts
+++ server/db/usersDB.ts
@@ -5,7 +5,7 @@ export function getUser(db = connection, id: string): Promise<user> {
   return db('users').where('auth0_id', id).select().first()
 }
 
-export function addUser(db = connection, input: user_draft) {
+export function addUser(input: user_draft, db = connection) {
   const { username, name, bio } = input
   return db('users').insert({ username, name, bio })
 }
