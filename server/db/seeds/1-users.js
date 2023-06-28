90bfd7b843c66c62b613eea3eba4e893273b7f8b
Andrew <andrewjtrotter@gmail.com>
2023-06-29 09:51:26 +1200

    derfasdf

--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('users').insert([
         {auth0_id: '1', username: 'khailjuice', name: 'Mikhail', bio: 'Fun loving guy'},
-        {auth0_id: '2', username: 'andyroo', name: 'Andrew', bio: 'Guy loving fun'},
+        {auth0_id: 'google-oauth2|117005350284520001031', username: 'andyroo', name: 'Andrew', bio: 'Guy loving fun'},
         {auth0_id: '3', username: 'khailjuice', name: 'Maia', bio: 'Fun loving guy'},
         {auth0_id: '4', username: 'khailjuice', name: 'Rocio', bio: 'Fun loving guy'},
         {auth0_id: '5', username: 'khailjuice', name: 'Steve', bio: 'Fun loving guy'},
