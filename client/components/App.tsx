c2cd8bae1e4f36c230a54f3f92de019c79027037
Andrew <andrewjtrotter@gmail.com>
2023-06-28 14:39:22 +1200

    integrated auth0

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,18 +1,88 @@
 import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks'
 import { fetchFruits } from '../slices/fruits'
+import { useAuth0 } from '@auth0/auth0-react'
+
+function LoginButton() {
+  const { loginWithRedirect } = useAuth0()
+
+  return (
+    <button
+      onClick={() => {
+        loginWithRedirect()
+      }}
+    >
+      Login
+    </button>
+  )
+}
+
+function RegisterButton() {
+  const { loginWithRedirect } = useAuth0()
+
+  return (
+    <button
+      onClick={() => {
+        loginWithRedirect({
+          authorizationParams: {
+            screen_hint: 'signin',
+            // redirect_uri: '/',
+          },
+        })
+      }}
+    >
+      Register
+    </button>
+  )
+}
+
+function LogoutButton() {
+  const { logout } = useAuth0()
+  return (
+    <button
+      onClick={() => {
+        logout()
+      }}
+    >
+      Logout
+    </button>
+  )
+}
 
 function App() {
+  const {
+    user,
+    getAccessTokenSilently,
+    loginWithRedirect,
+    logout,
+    isAuthenticated,
+  } = useAuth0()
   const fruits = useAppSelector((state) => state.fruits)
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch(fetchFruits())
   }, [dispatch])
 
+  async function handleAccessToken() {
+    const token = await getAccessTokenSilently()
+    console.log(token)
+  }
+
   return (
     <>
       <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <button onClick={handleAccessToken}>Show Access token</button>
+        {isAuthenticated ? (
+          <>
+            <LogoutButton />
+          </>
+        ) : (
+          <>
+            <LoginButton />
+            <RegisterButton />
+          </>
+        )}
         <ul>
           {fruits.map((fruit) => (
             <li key={fruit}>{fruit}</li>
--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,31 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { Auth0Provider } from '@auth0/auth0-react'
+
 import store from './store'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   )
 })
