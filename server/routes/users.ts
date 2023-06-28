979de2c16a584a2dbb8495f1b34713c3b7fbbd8f
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-29 11:24:43 +1200

    create post route for adding user

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -2,10 +2,25 @@ import { Router } from 'express'
 
 import * as db from '../db/usersDB'
 import { validateAccessToken } from '../auth0'
+import { user_draft_schema } from '../../models/user_models'
 
 const router = Router()
 
 export default router
+
+router.post('/', validateAccessToken, async (req, res) => {
+  try {
+    const input = req.body
+    const userData = user_draft_schema.parse(input)
+    const user = await db.addUser(userData)
+    res.json(user)
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+})
 
 // router.get('/', async (req, res) => {
 //   try {
