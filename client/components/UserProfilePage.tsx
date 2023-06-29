a4bc75064fbd57e64c55a89fd0fb1e720739f6a3
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-29 16:49:56 +1200

    create UserProfilePage

--- client/components/UserProfilePage.tsx
+++ client/components/UserProfilePage.tsx
@@ -1,35 +1,57 @@
 import { FormEvent, useState } from 'react'
-import { user_draft } from '../../models/user_models'
+import { user_draft, user } from '../../models/user_models'
 import UserProfileForm from './UserProfileForm'
-
-// interface Props {
-//   loadWidgets: () => void
-// }
+import { useNavigate } from 'react-router-dom'
+import { useMutation } from 'react-query'
+import { useAuth0 } from '@auth0/auth0-react'
+import useFetchUser from '../hooks/useFetchProfile'
+import { insertProfile } from '../apis/user'
 
 function UserProfilePage() {
-  const [userData, setUserData] = useState({
-    name: '',
-    username: '',
-    bio: '',
-  } as user_draft)
+  // const navigate = useNavigate()
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0()
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setUserData({
-      ...userData,
-      [event.target.name]: event.target.value,
-    })
+  const userQuery = useFetchUser()
+  const mutation = useMutation({
+    mutationFn: ({ form, token }: { form: user_draft | user; token: string }) =>
+      insertProfile(form, token),
+    onSuccess: () => {
+      // navigate('/home')
+    },
+  })
+
+  if (isLoading) {
+    return <div>Loading ...</div>
   }
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault()
-    //add api function for posting info to db e.g. addUser
-    //either use useNavigate to go to user home page or link
+  if (!isAuthenticated && !user) {
+    return <div>Not authenticated</div>
+  }
+
+  // const [userData, setUserData] = useState({
+  //   name: '',
+  //   username: '',
+  //   bio: '',
+  // } as user_draft)
+
+  // function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  //   setUserData({
+  //     ...userData,
+  //     [event.target.name]: event.target.value,
+  //   })
+  // }
+
+  async function handleSubmit(form: user_draft | user) {
+    const token = await getAccessTokenSilently()
+    mutation.mutate({ form, token })
+    // navigate('/home')
   }
 
   return (
-    <>
-      <UserProfileForm />
-    </>
+    <div>
+      <UserProfileForm handleSubmit={handleSubmit} profile={userQuery.data} />
+    </div>
   )
 }
 
