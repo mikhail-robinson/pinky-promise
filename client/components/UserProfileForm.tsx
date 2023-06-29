0fa4df4ffde4490c51fa51cfe0afb06ebf819431
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-29 16:49:42 +1200

    create userProfileForm

--- client/components/UserProfileForm.tsx
+++ client/components/UserProfileForm.tsx
@@ -1,15 +1,62 @@
-function UserProfileForm() {
+import { user, user_draft } from '../../models/user_models'
+
+interface Props {
+  profile?: user
+  handleSubmit: (profile: user | user_draft) => void
+}
+
+function UserProfileForm(props: Props) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const name = formData.get('name') as string
+    const username = formData.get('username') as string
+    const bio = formData.get('bio') as string
+
+    const form = {
+      name: name,
+      username: username,
+      bio: bio,
+    }
+
+    props.handleSubmit(form)
+  }
+
   return (
-    <form id="form">
+    <form onSubmit={handleSubmit}>
       <div>
+        <label htmlFor="name">Name</label>
         <input
           type="text"
           name="name"
-          placeholder=" "
-          value={form.name}
-          onChange={handleChange}
+          id="name"
+          required
+          defaultValue={props.profile?.name}
         />
-        <label htmlFor="name">Full Name</label>
+      </div>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          required
+          defaultValue={props.profile?.username}
+        />
+      </div>
+      <div>
+        <label htmlFor="bio">Bio</label>
+        <input
+          type="text"
+          name="bio"
+          id="bio"
+          required
+          defaultValue={props.profile?.bio}
+        />
+      </div>
+      <div>
+        <button>Save</button>
       </div>
     </form>
   )
