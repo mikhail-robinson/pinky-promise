038a2b5ed28a4eb3a8f817f94cf4ffb490178aee
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-29 17:22:52 +1200

    remove unused code from App.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,46 +1,11 @@
-import { useMutation, useQuery, useQueryClient } from 'react-query'
-import LoginButton from './LoginButton'
 import { useAuth0 } from '@auth0/auth0-react'
+import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
-import { useNavigate } from 'react-router-dom'
 import RegisterButton from './RegisterButton'
-import { Pledge } from '../../models/promise_models'
-import { useState } from 'react'
+import UserProfilePage from './UserProfilePage'
 
 function App() {
-  const [form, setForm] = useState()
-  const queryClient = useQueryClient()
-  const navigate = useNavigate()
-  const {
-    user,
-    getAccessTokenSilently,
-    loginWithRedirect,
-    logout,
-    isAuthenticated,
-  } = useAuth0()
-  // promisesQuery.data contains data and promisesQuery.isLoading is a boolean for a pending promise waiting to be fulfilled
-  // const promisesQuery = useQuery(['getPromises'], async () => {
-  //   return await getPromises() as promise[]
-  // })
-
-  // // example for mutation which is the useDispatch replacement.
-  // // mutation is a function that takes information to POST.
-  // const mutation = useMutation({
-  //   addNewPromise: ({ form: PromiseDraft; token: string }) =>
-  //     addPromise(form, token),
-  //   onSuccess: () => {
-  //     //tells the getPromise query to update
-  //     queryClient.invalidateQueries('getPromise')
-  //     // afterwards can navigate to another page
-  //     navigate('/')
-  //   },
-  // })
-
-  // function handleSubmit(event: FormEvent) {
-  //   event.preventDefault()
-  //   const token = await getAccessTokenSilently()
-  //   mutation.mutate(form, token)
-  // }
+  const { isAuthenticated } = useAuth0()
 
   return (
     <>
