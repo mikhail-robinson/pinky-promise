94616ef6c900e9d6faaa08cd3681291c5c3a767b
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-29 15:12:00 +1200

    replace addUser DB function

--- server/db/dataBaseFunctions/usersDB.ts
+++ server/db/dataBaseFunctions/usersDB.ts
@@ -1,6 +1,11 @@
 import connection from '../connection'
-import { user } from '../../../models/user_models'
+import { user, user_draft } from '../../../models/user_models'
 
 export function getUser(db = connection, id: string): Promise<user> {
   return db('users').where('auth0_id', id).select().first()
+}
+
+export function addUser(input: user_draft, db = connection) {
+  const { username, name, bio } = input
+  return db('users').insert({ username, name, bio })
 }
