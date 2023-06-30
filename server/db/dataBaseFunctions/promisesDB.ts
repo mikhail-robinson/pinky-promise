4ab262b73304a35a41b0253499df14e44786b5e2
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-06-30 16:44:00 +1200

    start db function

--- server/db/dataBaseFunctions/promisesDB.ts
+++ server/db/dataBaseFunctions/promisesDB.ts
@@ -8,3 +8,16 @@ export function getAllPromises(db = connection): Promise<Pledge[]> {
 export function getPromiseById(db = connection, id: string): Promise<Pledge[]> {
   return db('promises').where('id', id).select('promise_name as promiseName')
 }
+
+export async function getAllPromisesById(db = connection, userId: string) {
+  return await db('promises').select('promise_name as promiseName')
+}
+
+table.increments('id')
+table.string('promise_name')
+table.string('promise_description')
+table.string('user_id').references('users.auth0_id')
+table.string('friend_user_id').references('users.auth0_id')
+table.string('status')
+table.string('date_created')
+table.string('date_due')
