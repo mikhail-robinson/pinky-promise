d89e0d199b2525be4ca3c39480176b2a6c742578
Jordan-Lowe <jordanlowe659@gmail.com>
2023-07-01 01:12:51 +1200

    Added editProfilePage.tsx but needs more editing

---
+++ client/components/EditProfilePage.tsx
@@ -0,0 +1,46 @@
+import { FormEvent, useState } from 'react'
+import { UserDraft, User } from '../../models/user_models'
+import UserProfileForm from './UserProfileForm'
+import { useNavigate } from 'react-router-dom'
+import { useMutation } from 'react-query'
+import { useAuth0 } from '@auth0/auth0-react'
+import useFetchUser from '../hooks/useFetchProfile'
+import { updateProfile } from '../apis/user'
+
+function EditProfilePage() {
+  const navigate = useNavigate()
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0()
+
+  const userQuery = useFetchUser()
+  const mutation = useMutation({
+    mutationFn: ({ form, token }: { form: UserDraft | User; token: string }) =>
+      updateProfile(form, token),
+    onSuccess: () => {
+      navigate('/my-promises')
+    },
+  })
+
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
+
+  if (!isAuthenticated && !user) {
+    return <div>Not authenticated</div>
+  }
+
+  async function handleSubmit(form: UserDraft | User) {
+    const token = await getAccessTokenSilently()
+    mutation.mutate({ form, token })
+
+    navigate('/my-promises')
+  }
+
+  return (
+    <div>
+      <UserProfileForm handleSubmit={handleSubmit} profile={userQuery.data} />
+    </div>
+  )
+}
+
+export default EditProfilePage
