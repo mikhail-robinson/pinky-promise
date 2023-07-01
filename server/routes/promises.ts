39a618e23e451d1bcebb499a97d6e59728e81795
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-01 12:34:11 +1200

    create route for getAllPromisesById

--- server/routes/promises.ts
+++ server/routes/promises.ts
@@ -5,5 +5,23 @@ import { validateAccessToken } from '../auth0'
 
 const router = Router()
 
+router.get('/', validateAccessToken, async (req, res) => {
+  const auth0Id = req.auth?.payload.sub
+
+  if (!auth0Id) {
+    res.status(400).json({ message: 'Please provide an id' })
+    return
+  }
+
+  try {
+    const user = await db.getAllPromisesById(auth0Id)
+    res.status(200).json(user)
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .json({ message: 'Unable to retrieve promises from database' })
+  }
+})
 
 export default router
