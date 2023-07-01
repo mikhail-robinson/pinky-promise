684a3f49b4ba01087a0239c31d3414152be4348b
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-01 15:47:09 +1200

    Create components for All Promises

---
+++ client/components/AllPromisesItem.tsx
@@ -0,0 +1,25 @@
+import { PledgeFrontEnd } from '../../models/pledge_models'
+
+interface Props {
+  promises: PledgeFrontEnd[]
+}
+
+function AllPromisesItem(props: Props) {
+  const { promises } = props
+
+  return (
+    <>
+      <h2>Your Promises</h2>
+      {promises.map((promise) => {
+        return (
+          <li key={promise.promiseId}>
+            <div>{promise.promiseName}</div>
+            <div>{promise.friendName}</div>
+          </li>
+        )
+      })}
+    </>
+  )
+}
+
+export default AllPromisesItem
