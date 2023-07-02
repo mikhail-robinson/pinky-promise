2f699d9bf640c00792d39e23faec55248473b4a0
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-02 21:34:16 +1200

    create post route for addFriend

--- server/routes/friends.ts
+++ server/routes/friends.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import * as db from '../db/dataBaseFunctions/friendsDB'
 import { validateAccessToken } from '../auth0'
+import { friendsDraftSchema } from '../../models/friends_models'
 
 const router = Router()
 
@@ -13,10 +14,33 @@ router.get('/', validateAccessToken, async (req, res) => {
   try {
     const friends = await db.getAllFriendsById(auth0Id as string)
     res.status(200).json(friends)
-    
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: 'unable to retrieve data' })
+  }
+})
+
+router.post('/', validateAccessToken, async (req, res) => {
+  const input = req.body
+  try {
+    const friendDraftResult = friendsDraftSchema.safeParse(input)
+
+    if (!friendDraftResult.success) {
+      res.status(400).json({ message: 'Invalid form' })
+      return
+    }
+    const auth0Id = req.auth?.payload.sub
+    const userData = friendsDraftSchema.parse(input)
+
+    if (friendDraftResult.success && auth0Id) {
+      const user = await db.addFriend(userData)
+      res.status(201).json(user)
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 })
 
