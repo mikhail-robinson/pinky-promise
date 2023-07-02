fdff1681fa1c9ecde0e86803731b73d5b4e3d322
Rocio-Peralta <1rocioperalta@gmail.com>
2023-07-03 11:49:39 +1200

    finished styling my-promises

--- client/components/AllPromisesItem/AllPromisesItem.tsx
+++ client/components/AllPromisesItem/AllPromisesItem.tsx
@@ -15,20 +15,37 @@ function AllPromisesItem(props: Props) {
 
   return (
     <>
-      <h2>Your Promises</h2>
-      {promises.map((promise) => {
-        return (
-          //TODO add button to details page
-          <li key={promise.promiseId}>
-            <div>{promise.promiseName}</div>
-            <div>{promise.friendName}</div>
-            <button
-              className="fa-solid fa-angle-right"
-              onClick={() => redirectToDetailsPage(promise.promiseId)}
-            ></button>
-          </li>
-        )
-      })}
+      <div className="mt-20 w-80 ml-10">
+        <h2 className="font-secondary text-slate-50 text-2xl font-medium pb-4">
+          Your Promises
+        </h2>
+
+        <div className="flex flex-col gap-4">
+          {promises.map((promise) => {
+            return (
+              //TODO add button to details page
+              <div key={promise.promiseId} className="flex">
+                <ul className="list-disc">
+                  <div className="flex-initial p-2 w-72 text-base  text-slate-50 bg-slate-950 bg-opacity-25 rounded-l-lg font-body ">
+                    <div className="">
+                      <div className="">{promise.promiseName}</div>
+                      <span className=" text-bold text-gray-500">
+                        {promise.friendName}
+                      </span>
+                    </div>
+                  </div>
+                </ul>
+                <div className="flex-initial flex items-center justify-center w-10  text-base  text-slate-50 bg-slate-950 bg-opacity-25 rounded-r-lg font-body">
+                  <button
+                    className="fa-solid fa-angle-right"
+                    onClick={() => redirectToDetailsPage(promise.promiseId)}
+                  ></button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </>
   )
 }
