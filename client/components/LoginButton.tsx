9b3814e4a597608d34accaa25b4d6a103d5d51d1
Rocio-Peralta <1rocioperalta@gmail.com>
2023-07-04 10:37:17 +1200

    accepted login button Changes

--- client/components/LoginButton.tsx
+++ client/components/LoginButton.tsx
@@ -6,7 +6,7 @@ function LoginButton() {
   return (
     <div className="flex justify-start items-start">
       <button
-        className="font-body text-purple bg-pink text-2xl hover:bg-darkPink py-2 px-4 p-2 rounded-lg -mt-32 ml-14"
+        className="font-body text-purple bg-pink text-2xl hover:bg-darkPink py-2 px-4 p-2 rounded-lg -mt-32 ml-16"
         onClick={() => {
           loginWithRedirect({
             authorizationParams: {
