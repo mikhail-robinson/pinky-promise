1f8f0f2a2fd56a53de8e91fd83ed314a8dae9ef7
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-03 15:43:15 +1200

    add re render of page on click

--- client/components/AddFriendsItems/AddFriendsItem.tsx
+++ client/components/AddFriendsItems/AddFriendsItem.tsx
@@ -1,19 +1,34 @@
+import { useMutation, useQueryClient } from 'react-query'
 import { FriendNames } from '../../../models/friends_models'
 import { addFriend } from '../../apis/friends'
 import { useAuth0 } from '@auth0/auth0-react'
+
 interface Props {
   friends: FriendNames[]
 }
 
 function AddFriendsItem(props: Props) {
   const { friends } = props
-
   const { getAccessTokenSilently } = useAuth0()
+  const queryClient = useQueryClient()
 
-  async function handleClick(friendUserId: string) {
-    const token = await getAccessTokenSilently()
+  const addFriendMutation = useMutation(
+    async (friendUserId: string) => {
+      const token = await getAccessTokenSilently()
+      await addFriend(friendUserId, token)
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('getNotFriends')
+      },
+      onError: (error) => {
+        console.log('Error adding friend:', error)
+      },
+    }
+  )
 
-    await addFriend(friendUserId, token)
+  const handleClick = (friendUserId: string) => {
+    addFriendMutation.mutate(friendUserId)
   }
 
   return (
