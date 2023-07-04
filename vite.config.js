c800da0c2367e7771bc9d51851df655a5a1a6cd7
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-04 16:09:11 +1200

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { splitVendorChunkPlugin } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     proxy: {
-      '/api': `http://0.0.0.0:${process.env.PORT || 3000}`,
-      changeOrigin: true,
+      '/api': {
+        target: `http://0.0.0.0:${process.env.PORT || 3000}`,
+        changeOrigin: true,
+      },
     },
   },
 })
