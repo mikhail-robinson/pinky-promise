25be34876ffde12f6cbce6896e8eb7bfb059eb3b
Andrew <andrewjtrotter@gmail.com>
2023-07-05 11:45:47 +1200

    finished final touches

--- client/components/UserProfileForm.tsx
+++ client/components/UserProfileForm.tsx
@@ -27,15 +27,15 @@ function UserProfileForm(props: Props) {
     <>
       <div className="flex mt-20 justify-center h-screen">
         <form onSubmit={handleSubmit} className="w-80 h-auto">
-          <h1 className="font-secondary text-slate-50 text-2xl font-medium pb-4">
+          <h1 className="font-sans text-slate-50 text-2xl font-bold pb-4">
             {props.profile ? 'Your Profile' : 'Introduce Yourself'}
           </h1>
           <div className="pb-4">
-            <label className="font-secondary text-slate-50 pb-2" htmlFor="name">
+            <label className="font-sans font-bold text-slate-50 pb-2" htmlFor="name">
               Name
             </label>
             <input
-              className="p-2 w-full text-sm text-slate-50 bg-slate-950 bg-opacity-25 rounded-lg font-body"
+              className="p-2 w-full text-base font-bold text-fuchsia-200 bg-slate-950 bg-opacity-50 rounded-lg font-body"
               type="text"
               name="name"
               id="name"
@@ -45,11 +45,11 @@ function UserProfileForm(props: Props) {
             />
           </div>
           <div className="pb-4">
-            <label className="font-secondary text-slate-50" htmlFor="username">
+            <label className="font-sans font-bold text-slate-50" htmlFor="username">
               Username
             </label>
             <input
-              className="p-2 w-full text-sm text-slate-50 bg-slate-950 bg-opacity-25 rounded-lg font-body"
+              className="p-2 w-full text-base font-bold text-fuchsia-200 bg-slate-950 bg-opacity-50 rounded-lg font-body"
               type="text"
               name="username"
               id="username"
@@ -59,11 +59,11 @@ function UserProfileForm(props: Props) {
             />
           </div>
           <div className="text-start align-top place-items-start">
-            <label className="font-secondary text-slate-50 block" htmlFor="bio">
+            <label className="font-sans font-bold text-slate-50 block" htmlFor="bio">
               Bio
             </label>
             <textarea
-              className="p-2 pb-20 w-80 m text-sm text-slate-50 bg-slate-950 bg-opacity-25 rounded-lg font-body resize-none overflow-wrap-normal"
+              className="p-2 pb-20 w-80 m text-base font-bold text-fuchsia-200 bg-slate-950 bg-opacity-50 rounded-lg font-body resize-none overflow-wrap-normal"
               name="bio"
               id="bio"
               placeholder=""
