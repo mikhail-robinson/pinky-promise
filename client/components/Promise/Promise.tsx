f165f1be32db23af77a20ddae027fd9bc56e35fe
Maia <tarapipipim@gmail.com>
2023-07-05 10:32:40 +1200

    initial styling changes

--- client/components/Promise/Promise.tsx
+++ client/components/Promise/Promise.tsx
@@ -16,41 +16,55 @@ function Promise(props: Props) {
   //TODO: replace hardcoded date with acutal date
   const dateCreated = '28/06/3000'
   return (
-    <div>
+    <div className="font-sans center">
       <div className="promise">
-        <h1>{promiseName}</h1>
+        <h1 className="text-slate-50 font-bold text-base p-2 text-xl">
+          {promiseName}
+          <span className="text-base font-bold text-bold text-fuchsia-200 p-2">
+          {friendName}
+        </span>
+        </h1>
 
-        <div>{dateCreated}</div>
-        <div>{friendName}</div>
-        <p>{promiseDescription}</p>
+        <div className="text-base p-2 font-bold text-bold text-fuchsia-200">
+          {dateCreated}
+        </div>
+
+        <p className="text-slate-50 text-base p-2">{promiseDescription}</p>
 
         <button onClick={() => props.handleResolvePromise('broken')}>
-          <img src="/promiseBroken1.svg" alt="Promise Broken!" />
-          Promise Broken!
+          <img src="/promiseBroken.svg" alt="Promise Broken!" />
+          <span className="text-slate-50 font-bold text-base py">
+            Promise Broken!
+          </span>
         </button>
 
         <button onClick={() => props.handleResolvePromise('kept')}>
           <img src="/promiseKept.svg" alt="Promise Kept!" />
-          Promise Kept!
+          <span className="text-slate-50 font-bold text-base pb-2">
+            Promise Kept!
+          </span>
         </button>
-
-        <FacebookShareButton
-          // TO DO: update when app is deployed with deployment site
-          url={`kahikatea-2023-mikhail.au.auth0.com`}
-          title={`Check out my promise with, ${friendName}!`}
-          hashtag="#PinkyPromise"
-          name="Facebook Share Button"
-        >
-          <FacebookIcon className="ml-4" size={48} borderRadius={50} />
-        </FacebookShareButton>
-        <TwitterShareButton
-          title={`Check out my promise with, ${friendName}!`}
-          url={`${window.location.href}/my-promises/`}
-          hashtags={['PinkyPromise']}
-          name="Twitter Share Button"
-        >
-          <TwitterIcon className="ml-4" size={48} borderRadius={50} />
-        </TwitterShareButton>
+        <div className="flex">
+          <FacebookShareButton
+            // TO DO: update when app is deployed with deployment site
+            url={`kahikatea-2023-mikhail.au.auth0.com`}
+            title={`Check out my promise with, ${friendName}!`}
+            hashtag="#PinkyPromise"
+            name="Facebook Share Button"
+            className="items-center"
+          >
+            <FacebookIcon className="ml-4" size={48} borderRadius={50} />
+          </FacebookShareButton>
+          <TwitterShareButton
+            title={`Check out my promise with, ${friendName}!`}
+            url={`${window.location.href}/my-promises/`}
+            hashtags={['PinkyPromise']}
+            name="Twitter Share Button"
+            className="items-center"
+          >
+            <TwitterIcon className="ml-4" size={48} borderRadius={50} />
+          </TwitterShareButton>
+        </div>
       </div>
     </div>
   )
--- client/components/PromiseDetailPage/PromiseDetailPage.tsx
+++ client/components/PromiseDetailPage/PromiseDetailPage.tsx
@@ -46,15 +46,7 @@ function PromiseDetailPage() {
 
   return (
     <div className="flex justify-center mt-20">
-      <div className="flex flex-col bg-slate-950 bg-opacity-50 rounded-lg h-96">
-        {!promiseQuery.isLoading && promiseQuery.data && (
-          <Promise
-            promise={promiseQuery.data}
-            handleResolvePromise={handleResolvePromise}
-          />
-        )}
-      </div>
-      <div>
+      <div className="flex bg-slate-950 bg-opacity-50 rounded-lg h-96">
         {!promiseQuery.isLoading && promiseQuery.data && (
           <Promise
             promise={promiseQuery.data}
