0ddeaee61e235360d155b5b3ef2bee176698e6dc
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-04 16:03:31 +1200

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': `http://0.0.0.0:${process.env.PORT || 3000}`,
+      changeOrigin: true,
     },
   },
 })
