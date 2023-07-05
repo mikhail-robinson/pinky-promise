9a39d589ddef9b1c9ce609035d70685c90184610
Andrew <andrewjtrotter@gmail.com>
2023-07-05 15:21:39 +1200

    corrected centering of nav bar

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -19,8 +19,8 @@ function Nav() {
   return (
     <div className="app">
       <div className="fixed bottom-0 left-0 w-full h-20">
-        <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium pl-12">
-          <div className="w-16 h-16 bg bg-pink hover:bg-darkPink rounded-full flex items-center justify-center">
+        <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium px-10">
+          <div className="w-16 h-16 bg bg-pink hover:bg-darkPink mx-auto rounded-full flex items-center justify-center">
             <button
               onClick={() => goTo('/my-promises')}
               className="fa-solid fa-house fa-lg "
@@ -28,7 +28,7 @@ function Nav() {
             ></button>
           </div>
 
-          <div className="w-16 h-16 bg bg-pink hover:bg-darkPink rounded-full flex items-center justify-center">
+          <div className="w-16 h-16 bg bg-pink hover:bg-darkPink rounded-full mx-auto flex items-center justify-center">
             <button
               onClick={() => goTo('/add-promise')}
               className="fa-thin fa-plus fa-2xl mb-2"
@@ -39,7 +39,7 @@ function Nav() {
           <div className="">
             <button
               onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              className="w-16 h-16 bg bg-pink hover:bg-darkPink rounded-full flex items-center justify-center text-white focus:outline-none"
+              className="w-16 h-16 bg bg-pink hover:bg-darkPink rounded-full mx-auto flex items-center justify-center text-white focus:outline-none"
             >
               <i
                 className="fa-solid fa-bars fa-lg ml-0.5"
@@ -48,7 +48,7 @@ function Nav() {
             </button>
 
             {isDropdownOpen && (
-              <div className="fixed right-0  z-10 bottom-24   w-full rounded-lg bg-purple">
+              <div className="fixed right-0  z-10 bottom-24 w-full rounded-lg bg-purple">
                 <ul className="text-xl flex flex-col items-center">
                   <li className="w-full flex items-center justify-center rounded-lg px-4 py-6 text-slate-50  hover:bg-darkPink">
                     <a href="/my-profile">Profile</a>
